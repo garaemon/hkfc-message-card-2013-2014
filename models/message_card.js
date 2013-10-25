214b370c985d0be12582fe0d33d5576d530f4252
garaemon <garaemon@gmail.com>
2013-10-25 17:15:14 +0900

    updating MessageCard to have message1/2/3

--- models/message_card.js
+++ models/message_card.js
@@ -3,27 +3,33 @@ var mongoose = require("mongoose");
 
 
 var MessageCardSchema = libm.createSchema("MessageCard", {
-    name: String,
-    message: String,
-    location: String,
-    created_at: Date
+  name: String,
+  message1: String,
+  message2: String,
+  message3: String,
+  location: String,
+  created_at: Date
 });
 
 var MessageCard = mongoose.model("MessageCard");
 
 MessageCard.create = function(spec, cb) {
-    var name = spec.name;
-    var message_sentence = spec.message;
-    var location = spec.location;
-    var message = new MessageCard();
-    message.name = name;
-    message.message = message_sentence;
-    message.location = location;
-    message.saveAndUpdate(cb);
+  var name = spec.name;
+  var message1_sentence = spec.message1;
+  var message2_sentence = spec.message2;
+  var message3_sentence = spec.message3;
+  var location = spec.location;
+  var message = new MessageCard();
+  message.name = name;
+  message.message1 = message1_sentence;
+  message.message2 = message2_sentence;
+  message.message3 = message3_sentence;
+  message.location = location;
+  message.saveAndUpdate(cb);
 };
 
 MessageCard.prototype.saveAndUpdate = function(cb) {
-    var self = this;
-    self.updated_at = new Date();
-    self.save(cb);
+  var self = this;
+  self.updated_at = new Date();
+  self.save(cb);
 };
--- routes/register.js
+++ routes/register.js
@@ -4,10 +4,14 @@ var mongoose = require("mongoose");
 exports.post_url = "/register";
 exports.post = function(req, res, next) {
   var location = req.param("location");
-  var message = req.param("message");
+  var message1 = req.param("message1");
+  var message2 = req.param("message2");
+  var message3 = req.param("message3");
   var name = req.param("name");
   mongoose.model("MessageCard").create({
-    message: message,
+    message1: message1,
+    message2: message2,
+    message3: message3,
     location: location,
     name: name}, function(err) {
       if (err) {
