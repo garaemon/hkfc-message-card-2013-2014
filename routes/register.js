7783dd67e33b19604587deabc11c2a72d031eac7
garaemon <garaemon@gmail.com>
2013-10-25 17:13:45 +0900

    fixing indentation

--- routes/register.js
+++ routes/register.js
@@ -3,20 +3,19 @@ var mongoose = require("mongoose");
 
 exports.post_url = "/register";
 exports.post = function(req, res, next) {
-    var location = req.param("location");
-    var message = req.param("message");
-    var name = req.param("name");
-    mongoose.model("MessageCard").create({
-        message: message,
-        location: location,
-        name: name}, function(err) {
-            if (err) {
-                next(err);
-            }
-            else {
-                res.render("thanks_register", {});
-            }
-        });
+  var location = req.param("location");
+  var message = req.param("message");
+  var name = req.param("name");
+  mongoose.model("MessageCard").create({
+    message: message,
+    location: location,
+    name: name}, function(err) {
+      if (err) {
+        next(err);
+      }
+      else {
+        res.render("thanks_register", {});
+      }
+    });
 };
-
 
