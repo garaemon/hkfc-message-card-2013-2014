9e3516e1d7b43fc4c5a2807337eba704ae4bd37d
garaemon <garaemon@gmail.com>
2014-01-19 21:41:40 +0900

    updating images

--- routes/_index.js
+++ routes/_index.js
@@ -4,7 +4,62 @@ exports.get_url = "/";
 var LOCATIONS = [{
   value: "01matsuyama",
   name: "2013/10/26 松山サロンキティ"
+}, {
+  value: "02kouchi",
+  name: "2013/10/27 高知X-pt."
+}, {
+  value: "03okinawa",
+  name: "2013/11/02 沖縄桜坂セントラル"
+}, {
+  value: "04kobe",
+  name: "2013/11/09 神戸太陽と虎"
+}, {
+  value: "05okayama",
+  name: "2013/11/16 岡山CRAZYMAMA KINGDOM"
+}, {
+  value: "06takamatsu",
+  name: "2013/12/7 高松MONSTER"
+}, {
+  value: "07yokohama",
+  name: "2013/12/14 横浜BAYSIS"
+}, {
+  value: "08saitama",
+  name: "2013/12/15 さいたま新都心HEAVEN'S ROCK VJ-3"
+}, {
+  value: "09kooriyama",
+  name: "2013/12/26 郡山CLUB#9"
+}, {
+  value: "10sendai",
+  name: "2013/12/28 仙台MACANA"
+}, {
+  value: "11shimokitazawa",
+  name: "2014/01/04 下北沢GARDEN"
+}, {
+  value: "12fukuoka",
+  name: "2014/01/11 福岡BEAT STATION"
+}, {
+  value: "13hiroshima",
+  name: "2014/01/12 広島ナミキジャンクション"
+}, {
+  value: "14nagoya",
+  name: "2014/01/13 名古屋Electric Lady Land"
+}, {
+  value: "15sapporo",
+  name: "2014/01/25 札幌Sound Lab mole"
+}, {
+  value: "16umeda",
+  name: "2014/02/01 大阪umeda AKASO"
+}, {
+  value: "17kanazawa",
+  name: "2014/02/02 金沢vanvan V4"
+}, {
+  value: "18nigata",
+  name: "2014/02/08 新潟CLUB RIVERST"
+}, {
+  value: "19matsuyama",
+  name: "2014/02/16 松山サロンキティ"
 }];
+                
 
 exports.get = function(req, res, next) {
   var error = req.param("error");
